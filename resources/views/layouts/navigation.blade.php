ee75640da9dda206f19cc741d5eb2d33d4dc5149
devjames <devjames79@gmail.com>
2021-07-07 12:45:05 +0100

    change the avatar property

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -22,7 +22,7 @@
                     <x-slot name="trigger">
                         <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
                             <div class="flex items-center rounded-full h-6">
-                                <img class="rounded-full float-left h-full" src="https://randomuser.me/api/portraits/women/34.jpg"> <span class="ml-3">{{ Auth::user()->name }}</span>
+                                <img class="object-cover rounded-full float-left h-full " src="https://randomuser.me/api/portraits/women/34.jpg"> <span class="ml-3">{{ Auth::user()->name }}</span>
                             </div>
                             <div class="ml-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
