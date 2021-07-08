2b7257798c3cfd50d90d60065d1dfc2b7e48c52d
devjames <devjames79@gmail.com>
2021-07-08 22:16:13 +0100

    create the membros crud views

---
+++ resources/views/membros/criar.blade.php
@@ -0,0 +1,95 @@
+<x-app-layout>
+	<x-slot name="header">
+		<h2 class="font-semibold text-xl text-gray-800 leading-tight">
+				{{ __('Criar Membro') }}
+		</h2>
+	</x-slot>
+
+	<div class="py-6 px-4 container">
+		<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+			<div class="flex flex-col items-center justify-center">
+				<div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 w-full md:w-2/3 lg:w-1/2 ">
+					<div class="inline-block w-full py-2 align-middle ">
+						<!-- Validation Errors -->
+						<x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+						<form method="POST" action="{{ route('membros.store') }}">
+							@csrf
+
+							<!-- Name -->
+							<div>
+									<x-label for="nome" :value="__('Nome Completo')" />
+
+									<x-input id="nome" class="block mt-1 w-full" type="text" name="nome" :value="old('nome')" required autofocus />
+							</div>
+
+							<!-- Email Address -->
+							<div class="mt-4">
+									<x-label for="email" :value="__('Email')" />
+
+									<x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+							</div>
+							
+							<!-- Telemovel  -->
+							<div class="mt-4">
+									<x-label for="telemovel" :value="__('Telemovel')" />
+
+									<x-input id="telemovel" class="block mt-1 w-full" type="tel" name="telemovel" :value="old('telemovel')" required />
+							</div>
+
+							<!-- NIF -->
+							<div class="mt-4">
+									<x-label for="nif" :value="__('NIF')" />
+
+									<x-input id="nif" class="block mt-1 w-full"
+										type="text"
+										name="nif"
+										:value="old('nif')"
+										required />
+							</div>
+
+							<!-- Data de Nascimento -->
+							<div class="mt-4">
+									<x-label for="data_NS" :value="__('Data de Nascimento')" />
+
+									<x-input id="data_NS" class="block mt-1 w-full"
+										type="date"
+										name="data_NS"
+										:value="old('data_NS')"
+										required :value="old('data_NS')"/>
+							</div>
+							<!-- Genero -->
+							<div class="mt-4">
+								<x-label for="genero" :value="__('Sexo')" />
+								
+								<select name="genero" id="genero" class="block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" required v-model="old('genero')">
+									<option value="M">Masculino</option>
+									<option value="F">Feminino</option>
+								</select>
+							</div>
+							
+							<!-- Endereço -->
+							<div class="mt-4">
+									<x-label for="address" :value="__('Endereço')" />
+
+									<x-input id="address" class="block mt-1 w-full"
+																	type="text"
+																	name="address" required :value="old('eddress')"/>
+							</div>
+							<div class="flex items-center justify-between mt-4">
+									<a class="inline-flex items-center text-center px-4 py-2 text-sm bg-red-400 text-white rounded-md" href="{{ route('treinadores.index') }}">
+											{{ __('Cancelar') }}
+									</a>
+
+									<x-button class="ml-4">
+											{{ __('Guardar') }}
+									</x-button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</x-app-layout>
---
+++ resources/views/membros/index.blade.php
@@ -0,0 +1,101 @@
+<x-app-layout>
+	<x-slot name="header">
+		<h2 class="font-semibold text-xl text-gray-800 leading-tight">
+				{{ __('Membros') }}
+		</h2>
+	</x-slot>
+
+	<div class="py-6 px-4">
+		<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+			<div class="flex flex-col">
+				<div class="flex justify-between mb-4">
+					<p>Membros</p>
+					<x-action-button class="bg-primary-light w-9 h-9 md:w-40 space-x-2 rounded-full flex-shrink-0 justify-center" href="{{ route('membros.create') }}">
+						<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+						</svg>
+						<span class="hidden md:inline-flex">{{ __('Novo Membro') }}</span>
+					</x-action-button>
+				</div>
+				<div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+					<div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+						<div class="overflow-hidden border-b border-gray-200 rounded-md shadow-md">
+							<table class="min-w-full overflow-x-scroll divide-y divide-gray-200">
+								<thead class="bg-gray-50">
+									<tr>
+										<th
+											scope="col"
+											class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+										>
+											Nome
+										</th>
+										<th
+											scope="col"
+											class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+										>
+											NIF
+										</th>
+										<th
+											scope="col"
+											class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+										>
+											Telemovel
+										</th>
+										<th
+											scope="col"
+											class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+										>
+											Sexo
+										</th>
+										<th scope="col" class="relative px-6 py-3">
+											<span class="sr-only">Action</span>
+										</th>
+									</tr>
+								</thead>
+								<tbody class="bg-white divide-y divide-gray-200">
+									@foreach ($membros as $membro)
+										<tr class="transition-all hover:bg-gray-100 hover:shadow-lg">
+											<td class="px-6 py-4 whitespace-nowrap">
+												<div class="flex items-center">
+													<div class="flex-shrink-0 w-8 h-8 text-blue-300">
+														<svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 rounded-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+															<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+														</svg>
+													</div>
+													<div class="ml-4">
+														<div class="text-sm font-medium text-gray-900">{{ $membro->nome_completo }}</div>
+														<div class="text-sm text-gray-500">{{ $membro->email }}</div>
+													</div>
+												</div>
+											</td>
+											<td class="px-6 py-4 whitespace-nowrap">
+												<div class="text-sm text-gray-900">{{ $membro->NIF }}</div>
+											</td>
+											<td class="px-6 py-4 whitespace-nowrap">
+												<div
+													class="class="text-sm font-medium text-gray-900""
+												>
+													{{ $membro->telemovel }}
+												</div>
+											</td>
+											<td class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">{{ $membro->genero }}</td>
+											<td class="px-6 py-4 text-sm font-medium text-right whitespace-nowrap flex items-center space-x-3">
+												<a href="{{ route('membros.edit', $membro) }}" class="text-indigo-600 hover:text-indigo-900">Editar</a>
+												<form action="{{ route('membros.destroy', $membro) }}" method="POST" class="mr-1">
+													@csrf
+													@method('DELETE')
+													<button type="submit" class="text-blue-500">Remover</button>
+											</form>
+											</td>
+										</tr>
+									@endforeach
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</x-app-layout>
