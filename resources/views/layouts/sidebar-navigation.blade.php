1d6bcf11ed3bb92a28fc73d5a76b4f19beb55e77
devjames <devjames79@gmail.com>
2021-07-08 22:14:47 +0100

    update sidebar nav route to match the resource crud actions

--- resources/views/layouts/sidebar-navigation.blade.php
+++ resources/views/layouts/sidebar-navigation.blade.php
@@ -41,14 +41,14 @@
 			<span :class="{ 'lg:hidden': !isSidebarOpen }"" class="pl-4">{{ __('Dashboard') }}</span>
 		</x-sidebar-link>
 
-		<x-sidebar-link class="w-full" :href="route('treinadores.index')" :active="request()->routeIs('treinadores.index')">
+		<x-sidebar-link class="w-full" :href="route('treinadores.index')" :active="request()->routeIs('treinadores.*')">
 			<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2" />
 			</svg>
 			<span x-show="isSidebarOpen" class="pl-4">{{ __('Treinadores') }}</span>
 		</x-sidebar-link>
 
-		<x-sidebar-link class="w-full" :href="route('membros')" :active="request()->routeIs('membros')">
+		<x-sidebar-link class="w-full" :href="route('membros.index')" :active="request()->routeIs('membros.*')">
 			<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
 			</svg>
