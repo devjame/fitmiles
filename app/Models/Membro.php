38ff957e0fd73cd74ee3e166c2750d0d1e5e9348
devjames <devjames79@gmail.com>
2021-07-08 22:07:08 +0100

    create membro model

---
+++ app/Models/Membro.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Membro extends Authenticatable
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'membros';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nome_completo',
+        'email',
+        'NIF',
+        'telemovel',
+        'endereco',
+        'genero',
+        'data_de_nascimento',
+        'user_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
