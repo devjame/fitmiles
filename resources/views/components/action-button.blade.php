379b765a22378667b724b75ef1f1872ea769225c
devjames <devjames79@gmail.com>
2021-07-08 22:12:01 +0100

    make action button a link

--- resources/views/components/action-button.blade.php
+++ resources/views/components/action-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center bg-primary-light border border-transparent md:rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-primary-light active:bg-primary focus:outline-none focus:border-primary focus:ring ring-blue-300 disabled:opacity-25 transition ease-in-out duration-150']) }}>
+<a {{ $attributes->merge(['class' => 'inline-flex items-center bg-primary-light border border-transparent md:rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-primary-light active:bg-primary focus:outline-none focus:border-primary focus:ring ring-blue-300 disabled:opacity-25 transition ease-in-out duration-150']) }}>
     {{ $slot }}
-</button>
+</a>
