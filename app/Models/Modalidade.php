b198de212828d4072b1d68f3c5cbc4867a4d1ab5
devjames <devjames79@gmail.com>
2021-07-09 10:08:07 +0100

    create modalidades model

---
+++ app/Models/Modalidade.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Modalidade extends Authenticatable
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'modalidades';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nome',
+        'descricao',
+        'status',
+    ];
+}
