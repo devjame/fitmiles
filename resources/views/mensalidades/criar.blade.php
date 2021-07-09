8a87f2521cfcff33c465c3106e8136fc427e2a95
devjames <devjames79@gmail.com>
2021-07-09 15:30:22 +0100

    create mensalidade crud views

---
+++ resources/views/mensalidades/criar.blade.php
@@ -0,0 +1,73 @@
+<x-app-layout>
+	<x-slot name="header">
+		<h2 class="font-semibold text-xl text-gray-800 leading-tight">
+				{{ __('Novo Plano') }}
+		</h2>
+	</x-slot>
+
+	<div class="py-6 px-4 container">
+		<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+			<div class="flex flex-col items-center justify-center">
+				<div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 w-full md:w-2/3 lg:w-1/2 ">
+					<div class="inline-block w-full py-2 align-middle">
+						<!-- Validation Errors -->
+						<x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+						<form method="POST" action="{{ route('planos.store') }}">
+							@csrf
+
+							<!-- Name -->
+							<div>
+									<x-label for="nome" :value="__('Nome')" />
+
+									<x-input id="nome" class="block mt-1 w-full" type="text" name="nome" :value="old('nome')" required autofocus />
+							</div>
+
+							<!-- Descrição -->
+							<div class="mt-4">
+									<x-label for="descricao" :value="__('Descricão')" />
+
+									<textarea id="descricao" 
+									class="block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" 
+									name="descricao" :value="old('descricao')" required >
+
+									</textarea>
+							</div>
+
+							<!-- Preço -->
+							<div class="mt-4">
+									<x-label for="montante" :value="__('Preço')" />
+
+									<x-input id="montante" class="block mt-1 w-full" type="number" name="montante" :value="old('montante')" required autofocus />
+							</div>
+							
+							<!-- Duração -->
+							<div class="mt-4">
+								<x-label for="duracao" :value="__('Valido Até')" />
+								
+								<select name="duracao" id="duracao" class="block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" required v-model="old('duracao')">
+									<option value="30 dias" selected>1 mês</option>
+									<option value="90 dias">3 meses</option>
+									<option value="180 dias">6 meses</option>
+									<option value="360 dias">12 meses</option>
+								</select>
+							</div>
+							
+							<!-- Actions button-->
+							<div class="flex items-center justify-between mt-4">
+									<a class="inline-flex items-center text-center px-4 py-2 text-sm bg-red-400 text-white rounded-md" href="{{ route('modalidades.index') }}">
+											{{ __('Cancelar') }}
+									</a>
+
+									<x-button class="ml-4">
+											{{ __('Guardar') }}
+									</x-button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</x-app-layout>
---
+++ resources/views/mensalidades/index.blade.php
@@ -0,0 +1,106 @@
+<x-app-layout>
+	<x-slot name="header">
+		<h2 class="font-semibold text-xl text-gray-800 leading-tight">
+				{{ __('Planos') }}
+		</h2>
+	</x-slot>
+
+	<div class="py-6 px-4 container">
+		<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+			<div class="flex flex-col">
+				<div class="flex justify-between mb-4">
+					<p>Planos</p>
+					<x-action-button class="bg-primary-light w-9 h-9 md:w-44 rounded-full flex-shrink-0 justify-center" href="{{ route('planos.create') }}">
+						<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+						</svg>
+						<span class="hidden md:inline-flex">{{ __('Novo Plano') }}</span>
+					</x-action-button>
+				</div>
+				<div class="max-w-full -my-2 overflow-x-auto md:overflow-x-hidden	sm:-mx-6 lg:-mx-8">
+					<div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+						<div class="border-b border-gray-200 rounded-md shadow-md">
+							<table class="min-w-full divide-y divide-gray-200">
+								<thead class="bg-gray-50">
+									<tr>
+										<th
+											scope="col"
+											class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+										>
+											Nome
+										</th>
+										<th
+											scope="col"
+											class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+										>
+											Descrição
+										</th>
+										<th
+											scope="col"
+											class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+										>
+											Preço
+										</th>
+										<th
+											scope="col"
+											class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+										>
+											Validado Até
+										</th>
+										<th scope="col" class="relative px-6 py-3">
+											<span class="sr-only">Action</span>
+										</th>
+									</tr>
+								</thead>
+								<tbody class="bg-white divide-y divide-gray-200">
+									@foreach ($mensalidades as $mensalidade)
+										<tr class="transition-all hover:bg-gray-100 hover:shadow-lg">
+											<td class="px-6 py-4 whitespace-nowrap">
+												<div class="flex items-center">
+													{{-- <div class="flex-shrink-0 w-8 h-8 text-blue-300">
+														<svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 rounded-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+															<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+														</svg>
+													</div> --}}
+													<div>
+														<div class="text-sm font-medium text-gray-900">{{ $mensalidade->nome }}</div>
+													</div>
+												</div>
+											</td>
+											<td class="px-6 py-4">
+												<div class="text-sm text-gray-900">{{ $mensalidade->descricao }}</div>
+											</td>
+											<td class="px-6 py-4">
+													<span class="relative inline-block px-3 py-1 font-semibold leading-tight">
+														<span aria-hidden
+														class="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
+														<span class="relative">{{ number_format($mensalidade->montante, 2) }}</span>
+													</span>
+											</td>
+											<td class="px-6 py-4">
+												<div class="text-sm text-gray-900 capitalize">{{ $mensalidade->duracao }}</div>
+											</td>
+											<td class="px-6 py-4 text-sm font-medium text-right flex items-center flex-col  md:flex-row space-x-3">
+
+												<a href="{{ route('planos.edit', $mensalidade) }}" class="text-indigo-600 hover:text-indigo-900 mb-4 md:mb-0">Editar</a>
+
+												<form action="{{ route('planos.destroy', $mensalidade) }}" method="POST" class="mr-1">
+
+													@csrf
+													@method('DELETE')
+													<button type="submit" class="text-red-500">Remover</button>
+
+											</form>
+											</td>
+										</tr>
+									@endforeach
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</x-app-layout>
