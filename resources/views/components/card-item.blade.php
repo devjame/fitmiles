7a9a721b861b2281fd5825890082ff8e040a137f
devjames <devjames79@gmail.com>
2021-07-11 13:28:15 +0100

    add attribute binding

--- resources/views/components/card-item.blade.php
+++ resources/views/components/card-item.blade.php
@@ -11,7 +11,7 @@
 		</p>
 	</div>
 </div> --}}
-<div class="p-4 transition-shadow border rounded-lg shadow-sm hover:shadow-lg">
+<div class="p-4 transition-shadow border rounded-lg shadow-sm hover:shadow-lg" {{ $attributes }}>
 	<div class="flex items-start justify-between">
 		<div class="flex flex-col space-y-2">
 			<span class="text-gray-400">{{ $label }}</span>
@@ -22,7 +22,7 @@
 	</div>
 	</div>
 	<div>
-		<span class="inline-block px-2 text-sm text-white bg-green-300 rounded">14%</span>
-		<span>from 2019</span>
+		<span class="inline-block px-2 text-sm text-white bg-green-300 rounded"></span>
+		<span></span>
 	</div>
 </div>
