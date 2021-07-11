7812efcbad1be41e322b8d01f2a188924e9a7e7b
devjames <devjames79@gmail.com>
2021-07-11 13:26:09 +0100

    refactor card item with real data

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -20,7 +20,7 @@
 					Total Membros
 				</x-slot>
 				<x-slot name="data">
-					19238
+					{{ $membros }}
 				</x-slot>
 			</x-card-item>
 			<x-card-item>
@@ -33,20 +33,20 @@
 					Total Treinadores
 				</x-slot>
 				<x-slot name="data">
-					19
+					{{ $treinadores }}
 				</x-slot>
 			</x-card-item>
 			<x-card-item>
 				<x-slot name="icon">
 					<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
+						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
 					</svg>
 				</x-slot>
 				<x-slot name="label">
-					Novos Membros
+					Planos
 				</x-slot>
 				<x-slot name="data">
-					5
+					{{ $planos }}
 				</x-slot>
 			</x-card-item>
 		</div>
