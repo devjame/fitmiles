4b92d9583933700968446c1347d47a9bb750e2ae
devjames <devjames79@gmail.com>
2021-07-12 08:22:37 +0100

    change display layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,7 +17,7 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body>
-        <div class="font-sans text-gray-900 antialiased grid grid-cols-2">
+        <div class="font-sans text-gray-900 antialiased">
             {{ $slot }}
         </div>
     </body>
