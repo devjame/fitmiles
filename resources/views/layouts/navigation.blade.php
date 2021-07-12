9542e93b9c0294479f38d88e8756c9074cbaa835
devjames <devjames79@gmail.com>
2021-07-12 11:28:53 +0100

    fix style issues

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,6 +1,6 @@
 <nav class="bg-white border-b border-gray-100 shadow-sm">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="container mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex space-x-3">
                 <!-- Logo -->
