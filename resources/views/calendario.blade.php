2a4ca5b837e7dc80b71dcdd1be2c774c7ef97702
devjames <devjames79@gmail.com>
2021-07-13 21:41:01 +0100

    fix style issues

--- resources/views/calendario.blade.php
+++ resources/views/calendario.blade.php
@@ -6,7 +6,7 @@
 	</x-slot>
 
 	<div class="py-6 px-4">
-		<div class="container mx-auto sm:px-6 lg:px-8 flex flex-col items-center min-h-screen">
+		<div class="container mx-auto sm:px-6 lg:px-8 flex flex-col items-center min-h-scree">
 			<div id="calendar" class="w-full"></div>
 		</div>
 	</div>
